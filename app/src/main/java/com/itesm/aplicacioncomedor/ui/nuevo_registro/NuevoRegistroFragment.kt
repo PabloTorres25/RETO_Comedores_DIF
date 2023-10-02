1fa32b0c8b316bf7f85e9bf21c600e127ca718f1
PabloTorres25 <a01753412@tec.mx>
2023-10-01 18:30:33 -0600

    2023-10-01 06:30pm En proceso de poner un spinner

--- app/src/main/java/com/itesm/aplicacioncomedor/ui/nuevo_registro/NuevoRegistroFragment.kt
+++ app/src/main/java/com/itesm/aplicacioncomedor/ui/nuevo_registro/NuevoRegistroFragment.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.itesm.aplicacioncomedor.R
 import com.itesm.aplicacioncomedor.databinding.FragmentNuevoRegistroBinding
+import com.itesm.aplicacioncomedor.model.ToastUtil
 
-class NuevoRegistroFragment : Fragment() {
+class NuevoRegistroFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private var _binding: FragmentNuevoRegistroBinding? = null
 
@@ -29,28 +32,35 @@ class NuevoRegistroFragment : Fragment() {
 
         _binding = FragmentNuevoRegistroBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        // Mi codigo
+
         // Clic en btnEnviarRegistro
         binding.btnEnviarRegistro.setOnClickListener {
             findNavController().navigate(R.id.action_nav_nuevo_registro_to_nav_asistencia)
         }
-
-        // Clic en cbSoloRegistro
-        binding.cbSoloRegistro.setOnClickListener { view ->
-            if (binding.cbSoloRegistro.isChecked){
-                binding.cbPagado.visibility = View.GONE
-                binding.cbPresente.visibility = View.GONE
-            } else{
-                binding.cbPagado.visibility = View.VISIBLE
-                binding.cbPresente.visibility = View.VISIBLE
-            }
-        }
-        // Fin de Mi codigo
         return root
+    }
+
+    override fun onResume(){
+        super.onResume()
+        // Spinner Tipo de Asistencia
+        val adapter = ArrayAdapter.createFromResource(requireContext(), R.array.tipos_asistencia, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spTipoAsistencia.adapter = adapter
+        binding.spTipoAsistencia.onItemSelectedListener = this
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        // Obtener el elemento seleccionado y convertirlo a String
+        val selectedItem = parent?.getItemAtPosition(position)?.toString()
+        ToastUtil.mostrarToast(requireContext(), "Opcion seleccionada: $selectedItem")
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
