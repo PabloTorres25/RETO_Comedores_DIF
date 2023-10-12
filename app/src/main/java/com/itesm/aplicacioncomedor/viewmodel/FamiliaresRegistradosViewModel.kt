05b79370ee3028ec589a44663a32416be4b18ce8
PabloTorres25 <a01753412@tec.mx>
2023-10-11 20:12:56 -0600

    2023-10-11 08:12pm - Cambio algo en FamiliaresRegistrados

--- app/src/main/java/com/itesm/aplicacioncomedor/viewmodel/FamiliaresRegistradosViewModel.kt
+++ app/src/main/java/com/itesm/aplicacioncomedor/viewmodel/FamiliaresRegistradosViewModel.kt
@@ -1,7 +1,12 @@
 package com.itesm.aplicacioncomedor.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.itesm.aplicacioncomedor.model.asistencia.Asistentes
 
 class FamiliaresRegistradosViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _dataList = MutableLiveData<List<Asistentes>>() // Tu modelo de datos
+
+
 }
