3cddbcb83edeffa3fccd51a88785e32b1fd8c701
PabloTorres25 <a01753412@tec.mx>
2023-10-13 11:02:41 -0600

    2023-10-13 11:02pm - Intento de mostrar Asistentes en Familiares Registrados

--- app/src/main/java/com/itesm/aplicacioncomedor/view/FamiliaresRegistrados.kt
+++ app/src/main/java/com/itesm/aplicacioncomedor/view/FamiliaresRegistrados.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -13,7 +15,10 @@ import com.itesm.aplicacioncomedor.R
 import com.itesm.aplicacioncomedor.databinding.FragmentFamiliaresRegistradosBinding
 import com.itesm.aplicacioncomedor.model.ToastUtil
 import com.itesm.aplicacioncomedor.model.asistencia.Asistentes
+import com.itesm.aplicacioncomedor.model.asistencia.AsistentesData
+import com.itesm.aplicacioncomedor.view.asistencia.AdaptadorAsistentes
 import com.itesm.aplicacioncomedor.view.asistencia.AdaptadorRegistrosFamilia
+import com.itesm.aplicacioncomedor.viewmodel.AsistenciaVM
 import com.itesm.aplicacioncomedor.viewmodel.FamiliaresRegistradosViewModel
 
 
@@ -23,6 +28,7 @@ class FamiliaresRegistrados : Fragment() {
     private lateinit var viewModel: FamiliaresRegistradosViewModel
     var adaptadorRegistrosFamilia: AdaptadorRegistrosFamilia? = null
     lateinit var recyclerView: RecyclerView // Declara una propiedad para el RecyclerView
+    private val vm: AsistenciaVM by viewModels()    // Utilizamos el mismo viewModel que en Asistencia
 
 
     private val arrAsistentes = arrayOf(
@@ -45,26 +51,48 @@ class FamiliaresRegistrados : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FamiliaresRegistradosViewModel::class.java)
+        // viewModel = ViewModelProvider(this).get(FamiliaresRegistradosViewModel::class.java)
+        vm.listaAsistente.observe(viewLifecycleOwner) { listaCompleta ->
+            binding.etBuscadorFamiliasRegistradas.addTextChangedListener { editableText ->
+                val nombreFiltrado = editableText.toString()
+                val listaFiltrada = listaCompleta.filter {
+                    it.nombre.contains(nombreFiltrado, ignoreCase = true)
+                }
+                adaptadorRegistrosFamilia?.actualizarArreglo(listaFiltrada.toTypedArray())
+            }
+        }
+        configurarRV()
+        registrarEventos()
+    }
 
+    private fun registrarEventos() {
         // Fab registrados
         binding.fabFamiliaresRegistrados.setOnClickListener{
             // findNavController().navigate(R.id.action_familiaresRegistrados_to_familiaFragment)
             ToastUtil.mostrarToast(requireContext(), "Murcielago")
         }
+    }
 
-        configurarRV()
+
+    override fun onStart() {
+        super.onStart()
+        vm.registrarAsistentes()
     }
 
     private fun configurarRV() {
         val layout = LinearLayoutManager(requireContext())
         layout.orientation = LinearLayoutManager.VERTICAL
         recyclerView.layoutManager = layout
-        adaptadorRegistrosFamilia =
-            AdaptadorRegistrosFamilia(requireContext(), arrAsistentes) { onItemSelected(it) }
+
+        vm.listaAsistente.observe(viewLifecycleOwner){lista ->
+            val arrAsistentes = lista.toTypedArray()
+            adaptadorRegistrosFamilia = AdaptadorRegistrosFamilia(requireContext(), arrAsistentes) { onItemSelected(it) }
+        }
+
         recyclerView.adapter = adaptadorRegistrosFamilia
     }
-    fun onItemSelected(asitente: Asistentes){
+
+    fun onItemSelected(asistente: AsistentesData){
         ToastUtil.mostrarToast(requireContext(), "Algo esta pasando")
     }
 
--- app/src/main/java/com/itesm/aplicacioncomedor/view/asistencia/AdaptadorRegistrosFamilia.kt
+++ app/src/main/java/com/itesm/aplicacioncomedor/view/asistencia/AdaptadorRegistrosFamilia.kt
@@ -1,6 +1,8 @@
 package com.itesm.aplicacioncomedor.view.asistencia
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.icu.text.SimpleDateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,15 +10,18 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.itesm.aplicacioncomedor.R
 import com.itesm.aplicacioncomedor.model.asistencia.Asistentes
+import com.itesm.aplicacioncomedor.model.asistencia.AsistentesData
+import java.util.Calendar
+import java.util.Locale
 
-class AdaptadorRegistrosFamilia (private val contexto: Context, var arrAsistentes: Array<Asistentes>,
-                                private val onClickListener:(Asistentes) -> Unit)
-    : RecyclerView.Adapter<AdaptadorRegistrosFamilia.RenglonAsistente>()
-{
+class AdaptadorRegistrosFamilia (private val contexto: Context, var arrAsistentes: Array<AsistentesData>,
+                                private val onClickListener:(AsistentesData) -> Unit)
+    : RecyclerView.Adapter<AdaptadorRegistrosFamilia.RenglonAsistente>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RenglonAsistente {
         val vista = LayoutInflater.from(contexto).inflate(
             R.layout.familiares_registrados,
-            parent, false)
+            parent, false
+        )
         return RenglonAsistente(vista)
     }
 
@@ -26,21 +31,39 @@ class AdaptadorRegistrosFamilia (private val contexto: Context, var arrAsistente
     }
 
     override fun onBindViewHolder(holder: RenglonAsistente, position: Int) {
-        val asistente = arrAsistentes[position]
-        holder.set(asistente, onClickListener)
+        val asistentesData = arrAsistentes[position]
+        holder.set(asistentesData, onClickListener)
     }
 
-    fun actualizarArreglo(arrAsistentes: Array<Asistentes>){
+    @SuppressLint("NotifyDataSetChanged")
+    fun actualizarArreglo(arrAsistentes: Array<AsistentesData>) {
         this.arrAsistentes = arrAsistentes
         notifyDataSetChanged()
     }
 
-    class RenglonAsistente (var vistaRenglon: View) : RecyclerView.ViewHolder(vistaRenglon)
-    {
-        fun set(asistente: Asistentes, onClickListener:(Asistentes) -> Unit) {
-            vistaRenglon.findViewById<TextView>(R.id.tvNombreRegistrado).text = asistente.nombre
-            vistaRenglon.findViewById<TextView>(R.id.tvEdadRegistrado).text = asistente.edad.toString()
-            itemView.setOnClickListener{onClickListener(asistente)}
+    class RenglonAsistente(var vistaRenglon: View) : RecyclerView.ViewHolder(vistaRenglon) {
+
+        fun set(asistente: AsistentesData, onClickListener: (AsistentesData) -> Unit) {
+            val fecha = asistente.fechaNacimiento.substring(0, 10)
+
+            val formato = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+            val fechaNacimientoDate = formato.parse(fecha)
+
+            // Obtener la fecha actual
+            val calendarHoy = Calendar.getInstance()
+            val calendarNacimiento = Calendar.getInstance()
+            calendarNacimiento.time = fechaNacimientoDate
+
+            var edad = calendarHoy.get(Calendar.YEAR) - calendarNacimiento.get(Calendar.YEAR)
+
+            // Ajustar la edad si aún no ha cumplido años en este año
+            if (calendarHoy.get(Calendar.DAY_OF_YEAR) < calendarNacimiento.get(Calendar.DAY_OF_YEAR)) {
+                edad--
+
+                vistaRenglon.findViewById<TextView>(R.id.tvNombreRegistrado).text = asistente.nombre
+                vistaRenglon.findViewById<TextView>(R.id.tvEdadRegistrado).text = edad.toString()
+                itemView.setOnClickListener { onClickListener(asistente) }
+            }
         }
     }
 }
